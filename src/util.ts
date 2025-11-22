e8136dd1a76855a593c77a5679c8df3338b6dfdb
Adrien Avenia <49407726+adrienave@users.noreply.github.com>
2025-11-22 22:50:30 +0100

    Fix bug with difficulty not parsed as number

--- src/util.ts
+++ src/util.ts
@@ -4,6 +4,7 @@ export function parseCSVContent(rawCsv: string) {
   const result = Papa.parse(rawCsv, {
     header: true,
     skipEmptyLines: true,
+    dynamicTyping: true,
   });
 
   return result.data;
